3191e3baadb0cbf5d4c1307829ff36f6be318b10
Joshua M. Keyes <joshua.michael.keyes@gmail.com>
2025-06-25 00:28:49 -0700

    Add example Thrift IDL and update generated test bindings.

---
+++ src/test/thrift/ExampleService.thrift
@@ -0,0 +1,9 @@
+// ExampleService.thrift
+
+// Store everything under our example namespace.
+namespace java io.github.jmkeyes.spring.boot.thrift.example
+
+// A basic Thrift service to use in tests.
+service ExampleService {
+  void execute()
+}
