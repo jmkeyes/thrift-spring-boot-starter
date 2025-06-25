2859fb10e5ffb462c2035d89b0fbc7cda405256c
Joshua M. Keyes <joshua.michael.keyes@gmail.com>
2025-06-24 22:40:55 -0700

    Add missing Javadocs to ThriftController annotation.

--- src/main/java/io/github/jmkeyes/spring/boot/thrift/server/ThriftController.java
+++ src/main/java/io/github/jmkeyes/spring/boot/thrift/server/ThriftController.java
@@ -5,6 +5,9 @@ import org.apache.thrift.protocol.TProtocolFactory;
 
 import java.lang.annotation.*;
 
+/**
+ * An annotation to mark the class as a Thrift service controller.
+ */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
@@ -12,11 +15,15 @@ import java.lang.annotation.*;
 public @interface ThriftController {
     /**
      * At least one URL mapping under which this Thrift service should be available.
+     *
+     * @return At least one {@link String} path to mount the servlet at.
      */
     String[] value() default {};
 
     /**
      * The protocol factory this Thrift service should communicate with.
+     *
+     * @return The {@link TProtocolFactory} this Thrift service should use.
      */
     Class<? extends TProtocolFactory> protocolFactory() default TJSONProtocol.Factory.class;
 }
